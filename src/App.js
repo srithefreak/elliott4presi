b9dcfbcc73af3c5a4828f10ba429b235734f5cba
Sriyans Kanneganti <sriyansgamer@gmail.com>
2022-03-06 13:36:29 -0800

    we ready to deploy bois

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Pls vote Elliott 4 presi
         </p>
         <a
           className="App-link"
