6d0f9e07afa0f82580ac3abb5406329dd47cf9e1
Sriyans Kanneganti <sriyansgamer@gmail.com>
2022-03-27 18:38:09 -0700

    bruhh

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Pls vote Elliotte 4 presi
+          Pls vote Elliott 4 presi
         </p>
         <a
           className="App-link"
