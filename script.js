c9920d0d4b08569c5a9edfbd053cc53a7a2a8aa3
Sriyans Kanneganti <sriyansgamer@gmail.com>
2022-08-06 10:49:10 -0700

    Hamburger icon not returning to normal state after clicked Fixes #9
    Signed-off-by: Sriyans Kanneganti <sriyansgamer@gmail.com>

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-let prevScrollpos = window.scrollY;
-window.onscroll = function () {
-    let currentScrollPos = window.scrollY;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("top-nav-bar").style.top = "0";
-    } else {
-        document.getElementById("top-nav-bar").style.top = "-100px";
-    }
-    prevScrollpos = currentScrollPos;
-}
-
 function myFunction() {
     let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
+    if (x.className === "top-nav-bar") {
         x.className += " responsive";
     } else {
-        x.className = "topnav";
+        x.className = "top-nav-bar";
     }
 }
