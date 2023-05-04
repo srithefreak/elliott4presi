0199d640d8b0a2c812532a060c9d64280566bf65
Sriyans Kanneganti <sriyansgamer@gmail.com>
2023-05-04 04:31:43 +0000

    Got rid of unnecessary styling

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "top-nav-bar") {
-        x.className += " responsive";
-    } else {
-        x.className = "top-nav-bar";
-    }
-}
