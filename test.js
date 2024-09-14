512ee3dc37649f442d5454c5d5f3d4d78530dad6
Sriyans Kanneganti <sriyansgamer@gmail.com>
2024-09-13 18:41:01 -0700

    added test javascript file for vscode

---
+++ test.js
@@ -0,0 +1,12 @@
+function tets() {
+    if (2 > 1) {
+        console.log("tets");
+    } else {
+        console.log("esre");
+        if (3 > 1) {
+            console.log("fesfe");
+        }
+    }
+}
+
+tets();
